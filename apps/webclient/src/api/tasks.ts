5f83f3c2cc3bdbdae67ae7ed438a13d46c2f4851
rookiecol <juan.diegam@hotmail.com>
2023-06-08 23:54:09 -0500

    :pencil2: Removed unused task interface
    removed unused interface in api call, used a new type created based on task interface

--- apps/webclient/src/api/tasks.ts
+++ apps/webclient/src/api/tasks.ts
@@ -1,4 +1,4 @@
-import { CreateTask, Task } from "../utils/interfaces";
+import { CreateTask } from "../utils/interfaces";
 
 export async function createTaskRequest(task: CreateTask) {
     const response = await fetch('http://localhost:3000/api/tasks', {
